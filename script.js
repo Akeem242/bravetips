0db1e7797dca1a6608d5abd6c9e6f2438f2a23c2
Akeem242 <168595258+Akeem242@users.noreply.github.com>
2024-07-03 22:06:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("message")
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if(x.className==="navbar") {
+        x.className +=" responsive";
+    } else {
+        x.className = "navbar";
+    }
+} 
+  
